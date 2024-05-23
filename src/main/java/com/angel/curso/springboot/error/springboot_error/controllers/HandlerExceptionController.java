961a886284b3263e004c48f74013f42e9c0ae4e4
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-23 12:14:54 +0200

    DTO Error, @ControllerAdvice y Handler exception

---
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -0,0 +1,27 @@
+package com.angel.curso.springboot.error.springboot_error.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.angel.curso.springboot.error.springboot_error.models.Error;
+
+@RestControllerAdvice
+
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ ArithmeticException.class })
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        // return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+}
