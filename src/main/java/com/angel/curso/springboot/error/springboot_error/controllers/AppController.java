61b125a356ac7bbc42cb51731211b094c33e3f2a
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-23 12:55:24 +0200

    Exception NumberFormatException

--- src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/AppController.java
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
 
     @GetMapping("/app")
-    public String index(){
-        int value = 100/0;
+    public String index() {
+        // int value = 100 / 0;
+        int value = Integer.parseInt("10x");
         System.out.println(value);
         return "ok 200";
     }
--- src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.angel.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -32,10 +35,20 @@ public class HandlerExceptionController {
         error.setDate(new Date());
         error.setError("Api rest no encontrado");
         error.setMessage(e.getMessage());
-        
+
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(NumberFormatException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "Numero invalido o incorrecto, no tiene formato de dígito!");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
+    }
 }
