9656f04ae9309720bfcbbdcebe91caefdb75ba26
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-24 16:47:19 +0200

    Usando api Optional

--- src/main/java/com/angel/curso/springboot/error/springboot_error/services/UserService.java
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/services/UserService.java
@@ -1,11 +1,12 @@
 package com.angel.curso.springboot.error.springboot_error.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.angel.curso.springboot.error.springboot_error.models.domain.User;
 
 public interface UserService {
 
     List<User> findAll();
-    User findById(Long id);
+    Optional<User> findById(Long id);
 }
