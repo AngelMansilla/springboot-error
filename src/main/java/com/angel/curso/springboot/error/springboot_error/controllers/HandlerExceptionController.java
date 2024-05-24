fe516f5f44c0cb299ab5c85b1fff6782f5c43a07
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-24 16:33:42 +0200

    Creando clase Exception

--- src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,11 +6,13 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.angel.curso.springboot.error.springboot_error.exceptions.UserNotFoundException;
 import com.angel.curso.springboot.error.springboot_error.models.Error;
 
 @RestControllerAdvice
@@ -49,6 +51,19 @@ public class HandlerExceptionController {
         error.put("error", "Numero invalido o incorrecto, no tiene formato de dígito!");
         error.put("message", ex.getMessage());
         error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
+    }
+
+    @ExceptionHandler({ NullPointerException.class, HttpMessageNotWritableException.class, UserNotFoundException.class })
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> userNotFoundException(Exception ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", ex.getMessage());
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
         return error;
     }
 }
