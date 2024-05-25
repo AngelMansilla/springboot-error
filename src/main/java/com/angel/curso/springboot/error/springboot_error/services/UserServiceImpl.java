a09ad83e3fcc69227b195ee7c700b3324d2c8657
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-25 12:09:08 +0200

    Optimizando codigo con Api Stream

--- src/main/java/com/angel/curso/springboot/error/springboot_error/services/UserServiceImpl.java
+++ src/main/java/com/angel/curso/springboot/error/springboot_error/services/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.angel.curso.springboot.error.springboot_error.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.angel.curso.springboot.error.springboot_error.models.domain.User;
@@ -11,16 +11,8 @@ import com.angel.curso.springboot.error.springboot_error.models.domain.User;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private List<User> users;
-
-    public UserServiceImpl() {
-        this.users = new ArrayList<>();
-        users.add(new User(1L, "Pepe", "Gonzalez"));
-        users.add(new User(2L, "Pepa", "Perez"));
-        users.add(new User(3L, "Maria", "Rodriguez"));
-        users.add(new User(4L, "Rodrigo", "Garcia"));
-        users.add(new User(5L, "Ana", "Moraña"));
-    }
 
     @Override
     public List<User> findAll() {
@@ -29,14 +21,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<User> findById(Long id) {
-        User user = null;
-        for (User u : users) {
-            if (u.getId().equals(id)) {
-                user = u;
-                break;
-            }
-        }
-        return Optional.ofNullable(user);
+        return users.stream().filter(usr -> usr.getId().equals(id)).findFirst();
+        // for (User u : users) {
+        // if (u.getId().equals(id)) {
+        // user = u;
+        // break;
+        // }
+        // }
+        // return Optional.ofNullable(user);
     }
 
 }
